6cb619e167c602d8e4e9a478d4a4252bbab56733
sean <shanaesenderlingr@gmail.com>
2024-06-25 08:52:40 +0800

    add login

--- resources/user.py
+++ resources/user.py
@@ -2,19 +2,26 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256 # 哈希客戶端發送給我們的密碼
+
 from db import db
 from models import UserModel
 from schemas import UserSchema
 blp = Blueprint("Users", "users", description="Operations on users")
 
+# 註冊
+# 期望看到的格式
+# {
+# "username":"name",
+# "password":"1234"
+# }
 @blp.route("/register")
 class UserRegister(MethodView):
     @blp.arguments(UserSchema)
     def post(self, user_data):
-
+        # 鑒察 username 是否有重複
         # 在這裡，我們可以跳過這一點，稍後在插入資料庫時捕獲 IntegrityError。這可能是個更簡潔的解決方案。
         if UserModel.query.filter(UserModel.username == user_data["username"]).first():
-            abort(409, message="A user with that username already exists.")
+            abort(409, message="使用該用戶名的用戶已存在")
 
         # 獲取數據
         user = UserModel(
@@ -24,9 +31,28 @@ class UserRegister(MethodView):
 
         db.session.add(user) # 加入數據庫
         db.session.commit() # 更新數據庫
-        return {"message": "User created successfully."}, 201 # 返回訊息
+        return {"message": "用戶創建成功"}, 201 # 返回訊息
+
+# 登入
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+        # server 檢查收到的請求是否與資料庫一致
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            # 建立 jwt token
+            access_token = create_access_token(identity=user.id)
+            # 返回 token 到 client
+            return {"access_token":access_token}
+        
+        abort(401, message="無效驗證")
 
 
+
+# 這個在實際部屬時要刪除
 @blp.route("/user/<int:user_id>")
 class User(MethodView):
     @blp.response(200, UserSchema)
